10dc3cf3a6dd95b81a1d9dd0924c9323db2b1fa2
El-gibbor <chiagoziem11@yahoo.com>
2023-07-13 17:43:38 +0400

    str tokenising

--- pid.c
+++ pid.c
@@ -13,7 +13,7 @@ int main(void)
 	 *
 	 *  getppid() - gets the parent process ID of the current process(xcuting prog).
 	 *  ID is constant because it Idetifies a given child process regardless of
-	 *  how many times this prog xcutes, the PPID identifies that child with one ID
+	 *  how many times this prog xcutes, the PPID identifies that child with a unique ID
 	 */
 
 	pid_t thePid;
---
+++ strtok.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	/**
+	 * char *strtok(char *str, char *delim)
+	 * ------------------------------------
+	 * splits a given string from the specified delimeter.
+	 * delimeter can be a byte or more bytes/char that delimits the str.
+	 * after the first call to this func, subsequent call to split the next delimited
+	 * str takes in NULL as an argument in the str parameter - strtok(NULL, delim)
+	 *
+	 * Returns ptr to a tokenised null terminated str or NULL when all chars in
+	 * the str are all read.
+	 */
+	char *strToken, *deli = " ", s[] = "this is a string";
+
+	/**
+	 * strToken = strtok(s, deli);
+	 printf("%s\n", strToken);
+
+	//subsequent call
+	strToken = strtok(NULL, deli);
+	printf("%s\n", strToken);
+	*/
+
+	/*tokenising the entire string*/
+	strToken = strtok(s, deli);
+	while (strToken)
+	{
+		printf("%s\n", strToken);
+		strToken = strtok(NULL, deli); /*subsequent call*/
+	}
+	return (0);
+}
