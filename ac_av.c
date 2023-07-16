f4ee8a5d723e555627a9d36580beba1d41ad37e7
El-gibbor <chiagoziem11@yahoo.com>
2023-07-16 15:53:52 +0400

    prints all args without argc

---
+++ ac_av.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * concept page exercise
+ * ----------------------
+ * a program that prints all the arguments, without using ac
+*/
+
+int main(int ac, char **av)
+{
+	(void)ac;
+
+	while (*av)
+	{
+		printf("%s\n", *av);
+		*av++;
+	}
+	return (0);
+}
