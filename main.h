9a0b0c011677882a81fcb511d5fff84b155130fb
El-gibbor <chiagoziem11@yahoo.com>
2023-07-18 10:56:09 +0400

    declared Environ in header

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/***************** HEADER FILES ***************/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/************ Signature declarations ********/
+extern char **environ;
+
+#endif
