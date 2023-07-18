13771d3200be44132e31c978e0411acd5f39dcdf
El-gibbor <chiagoziem11@yahoo.com>
2023-07-18 16:21:26 +0400

    interative mode, its len & newline, solved

--- simple_shell.c
+++ simple_shell.c
@@ -1,54 +1,54 @@
 #include "main.h"
-
 /**
- * main - xcutes a unix cmdline interpreter
+ * main - Executes a Unix command-line interpreter.
  * @ac: Number of CLI args.
- * @av: array of pointers to CLI args
- * Return: 0
+ * @av: Array of pointers to CLI args.
+ * Return: 0.
  */
-int main(int ac, char *av[])
+int main(__attribute__((unused)) int ac, char *av[])
 {
-	char *cmd_line = NULL, **args = NULL;
 	size_t size_line;
 	pid_t child_pid;
-	int status, i;
+	int status, i, interactive_mode = isatty(STDIN_FILENO);
+	char *cmd_line = NULL, **args = NULL;
 
-	(void) ac;
-	for (; isatty(STDIN_FILENO);)
+	for (;;)
 	{
-		write(1, "#cisfun$ ", 9);
-
-		if (getline(&cmd_line, &size_line, stdin) < 0)
+		if (interactive_mode)
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+		if (getline(&cmd_line, &size_line, stdin) == -1)
+		{
+			if (interactive_mode)
+				write(STDOUT_FILENO, "\n", 1);
 			break;
-		if (*cmd_line == EOF)
-			exit(0);
-
+		}
 		cmd_line[_strlen(cmd_line) - 1] = '\0';
 
 		args = malloc(sizeof(char *) * 2);
 		if (!args)
-			perror("Error: ");
+			error_exit("Error: ");
 		args[0] = malloc(_strlen(cmd_line) + 1);
 		if (!args[0])
-			perror("Error: ");
-	
+			error_exit("Error: ");
+
 		_strcpy(args[0], cmd_line);
 		args[1] = NULL;
 
 		child_pid = fork();
 		if (child_pid < 0)
-			perror("Error: ");
+			error_exit("Error: ");
 		if (child_pid == 0)
 		{
 			execve(args[0], av, environ);
-			perror(av[0]);
-			exit(98);
+			perror(args[0]);
+			exit(EXIT_FAILURE);
 		}
-		wait(&status);
+		if (wait(&status) == -1)
+			error_exit("Error: ");
 		for (i = 0; i < 2; i++)
 			free(args[i]);
 	}
 	free(args);
 	free(cmd_line);
-	exit(0);
+	exit(EXIT_SUCCESS);
 }
