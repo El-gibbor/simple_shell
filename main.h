c92826f9c889e59c233239646f85adab48acc09e
El-gibbor <chiagoziem11@yahoo.com>
2023-07-18 19:25:41 +0400

    added func signatures

--- main.h
+++ main.h
@@ -1,16 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/***************** HEADER FILES ***************/
+/************* HEADER FILES **************/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 
-/************ Signature declarations ********/
+/********* Signature declarations ********/
 int _strlen(char *s);
 extern char **environ;
 char *_strcpy(char *dest, const char *src);
+void execute_cmd(char **args, char *av[]);
+void error_exit(const char *error);
 
 #endif
