525f33d06fca29e80f9e69bb0485c23a64c541e4
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-07-18 20:25:50 +0400

    func signature for prepar_args()

--- main.h
+++ main.h
@@ -8,11 +8,12 @@
 #include <string.h>
 #include <sys/wait.h>
 
-/********* Signature declarations ********/
+/********** Function Signatures *********/
 int _strlen(char *s);
 extern char **environ;
 char *_strcpy(char *dest, const char *src);
 void execute_cmd(char **args, char *av[]);
 void error_exit(const char *error);
+char **prepare_args(char *cmd_line);
 
 #endif
