240cdabcfba5384548b874e9a668f0fd4a758a1a
El-gibbor <chiagoziem11@yahoo.com>
2023-07-25 22:44:26 +0400

    all header files included

--- main.h
+++ main.h
@@ -1,19 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/************* HEADER FILES **************/
-#include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
-/********** Function Signatures *********/
-int _strlen(char *s);
-extern char **environ;
-char *_strcpy(char *dest, const char *src);
-void execute_cmd(char **args, char *av[]);
-void error_exit(const char *error);
-char **prepare_args(char *cmd_line);
 
 #endif
