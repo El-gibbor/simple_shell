d14dd73466e0c7a9838c0522f62bc5cf0b2808a0
El-gibbor <chiagoziem11@yahoo.com>
2023-07-26 01:22:49 +0400

    implemented strpbrk

--- str_handler.c
+++ str_handler.c
@@ -18,3 +18,26 @@ int _strncmp(const char *s1, const char *s2, unsigned int n)
 	}
 	return (*s1 - *s2);
 }
+
+/**
+ * strpbrk_no - locates the first occurrence in the string s
+ * of any bytes in the string accept
+ * @s: a string
+ * @accept: a string
+ * Return: the index of the byte in s that matches
+ * one of the bytes in accept;
+ */
+size_t strpbrk_no(const char *s, char *accept)
+{
+	char *p;
+	size_t pos;
+
+	for (pos = 0; s[pos]; pos++)
+	{
+		for (p = accept; *p; p++)
+			if (s[pos] == *p)
+				return (pos);
+	}
+	return (pos);
+}
+
