00b9f15abff53af4c9710b41d2365b8123888fb0
teflon40 <frimpong.bernard3220@gmail.com>
2023-07-26 17:55:58 +0000

    Executes commands

---
+++ execute.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * execute_cmd - executes command by calling execve in a child process
+ * @args: an array of pointers to strings / commands.
+ * @cmd: command status, whether found or not.
+ */
+void execute_cmd(char **args, cmd_t cmd_stat)
+{
+	pid_t child_pid;
+	int status;
+
+	if (cmd_stat == NOT_FOUND)
+	{
+		execve(args[0], args, environ);
+		perror(args[0]);
+		return;
+	}
+
+	child_pid = fork();
+	switch (child_pid)
+	{
+		case -1:
+			error_exit("fork");
+			break;
+		case 0:
+			execve(args[0], args, environ);
+			break;
+		default:
+			if (wait(&status) == -1)
+				error_exit("wait");
+			break;
+	}
+}
